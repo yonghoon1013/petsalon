a018d4cb57191dc143394af72a4eb7f8ab16458a
Git-JY <tlatlago824@gmail.com>
2023-11-29 14:24:09 +0900

    sdfsdf

---
+++ app/components/Context.js
@@ -0,0 +1,15 @@
+"use client"
+import React, { createContext } from 'react'
+
+export const myContext = createContext();
+
+function Context({children}) {
+
+  return (
+    <myContext.Provider>
+        {children}
+    </myContext.Provider>
+  )
+}
+
+export default Context
