dd27ece6a68012d2b24f2ed209e3f29ecf84b801
yonghoon1013 <jyh10105@gmail.com>
2023-12-01 15:45:50 +0900

    sf

---
+++ app/components/favorite/Favorite.js
@@ -0,0 +1,24 @@
+"use client"
+
+import React from 'react'
+
+function Favorite() {
+  return (
+    <>
+        <div className='titleWrapper'>
+            <h2 className='title'>내 관심 미용</h2>
+             <button className='serch'><img src='./serch.png'></img></button>
+             <button className='notice'><img src='./notice.png'></img></button>
+        </div>
+
+        <div className='mainWrapper'>
+            <p className='text'>관심 등록한 디자이너 & 미용실이 없네요.</p>
+            <button className='more'>인기 있는 미용 둘러보기</button>
+        </div>
+
+        
+    </>
+  )
+}
+
+export default Favorite
