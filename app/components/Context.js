b545d03d25c48e3583bffac8efd814522e851143
min <wolfbee700@gmail.com>
2023-12-01 15:12:50 +0900

    sfdsf

--- app/components/Context.js
+++ app/components/Context.js
@@ -4,7 +4,7 @@ import React, { createContext } from 'react'
 export const myContext = createContext();
 
 function Context({children}) {
-
+ 
   return (
     <myContext.Provider>
         {children}
